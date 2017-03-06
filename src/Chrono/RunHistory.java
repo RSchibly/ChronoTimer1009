d8d50b3d9bd2fdb80d1e05a498c6acee50601f9e
Will Paltz <blizzard@Wills-MacBook-Pro.local>
2017-03-05 18:27:26 -0600

    Changes during meeting 3/5, RIP Rick

--- src/Chrono/RunHistory.java
+++ src/Chrono/RunHistory.java
@@ -1,5 +1,28 @@
 package Chrono;
+import java.util.ArrayList;
 
 public class RunHistory {
-
+	private ArrayList<Run> runList;
+	
+	public RunHistory() {
+		//IS A GIVER, NOT A TAKER
+		runList = new ArrayList<Run>();
+	}
+	
+	public void addRun(Run r){
+		runList.add(r);
+	}
+	
+	public Run getRun(int runNumber){
+		return runList.get(runNumber - 1);
+	}
+	
+	public void clear(){
+		runList = new ArrayList<Run>();
+	}
+	
+	//TODO We may need this
+	public void fuckThisClearThisShit(Run DESTROY){
+		System.exit(0);
+	}
 }
